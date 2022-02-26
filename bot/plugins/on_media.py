d0c7b133b4e1e8cc4393cb7e8e654ec8b94ec6c5
Albin <95078987+Samantha-a@users.noreply.github.com>
2022-02-26 23:51:10 +0530

    Update on_media.py

--- bot/plugins/on_media.py
+++ bot/plugins/on_media.py
@@ -13,14 +13,25 @@ from pyrogram import (
 async def on_media_handler(c: Client, m: "types.Message"):
     if not m.from_user:
         return await m.reply_text("I don't know about you sar :(")
-    await add_user_to_database(c, m)
-    await asyncio.sleep(3)
-    await c.send_flooded_message(
-        chat_id=m.chat.id,
+    await bot.send_message(
+        chat_id=update.chat.id,
         text="**Should I show File Information?**",
-        reply_markup=types.InlineKeyboardMarkup(
-            [[types.InlineKeyboardButton("Yes", callback_data="showFileInfo"), types.InlineKeyboardButton("No", callback_data="closeMessage")]]
-        ),
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton('📝Rename', callback_data = "rnme"),
+                    InlineKeyboardButton('📂File To Video', callback_data = "f2v")
+                ],
+                [
+                    InlineKeyboardButton('🎞️Custom Thumbnail', callback_data = "cthumb"),
+                    InlineKeyboardButton('📑Custom Caption', callback_data = "ccaption")
+                ],
+                [
+                    InlineKeyboardButton('💬About', callback_data = "about")
+                ]
+            ]
+        )
+    )
         disable_web_page_preview=True,
         reply_to_message_id=m.message_id
     )
