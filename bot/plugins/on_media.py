2c4dfe1b8c4a70e3a3aae22f7ad35b9daa52a55b
Albin <95078987+Samantha-a@users.noreply.github.com>
2022-02-26 23:56:06 +0530

    Update on_media.py

--- bot/plugins/on_media.py
+++ bot/plugins/on_media.py
@@ -1,3 +1,5 @@
+# (c) @AbirHasan2005
+
 import asyncio
 from bot.client import Client
 from bot.core.db.add import (
@@ -13,25 +15,14 @@ from pyrogram import (
 async def on_media_handler(c: Client, m: "types.Message"):
     if not m.from_user:
         return await m.reply_text("I don't know about you sar :(")
-    await bot.send_message(
-        chat_id=update.chat.id,
+    await add_user_to_database(c, m)
+    await asyncio.sleep(3)
+    await c.send_flooded_message(
+        chat_id=m.chat.id,
         text="**Should I show File Information?**",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton('📝Rename', callback_data = "rnme"),
-                    InlineKeyboardButton('📂File To Video', callback_data = "f2v")
-                ],
-                [
-                    InlineKeyboardButton('🎞️Custom Thumbnail', callback_data = "cthumb"),
-                    InlineKeyboardButton('📑Custom Caption', callback_data = "ccaption")
-                ],
-                [
-                    InlineKeyboardButton('💬About', callback_data = "about")
-                ]
-            ]
-        )
-    )
+        reply_markup=types.InlineKeyboardMarkup(
+            [[types.InlineKeyboardButton("Yes", callback_data="showFileInfo"), types.InlineKeyboardButton("No", callback_data="closeMessage")]]
+        ),
         disable_web_page_preview=True,
         reply_to_message_id=m.message_id
     )
