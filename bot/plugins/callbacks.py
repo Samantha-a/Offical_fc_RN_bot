3c83a68c4f93168b72c4e4a0cde2eda4fe01df0f
Aadhi <87893573+Aadhi000@users.noreply.github.com>
2022-02-27 11:58:56 +0530

    Update callbacks.py

--- bot/plugins/callbacks.py
+++ bot/plugins/callbacks.py
@@ -1,118 +1,200 @@
-from pyrogram import types
+# (c) @AbirHasan2005
+
+import traceback
+from typing import Union
 from bot.client import Client
-from bot.core.db.database import db
-from bot.core.file_info import (
-    get_media_file_name,
-    get_media_file_size,
-    get_file_type,
-    get_file_attr
+from pyrogram import (
+    raw,
+    utils
 )
-from bot.core.display import humanbytes
-from bot.core.handlers.settings import show_settings
+from pyrogram.types import (
+    Message
+)
+from configs import Config
+from bot.core.utils.rm import rm_dir
+from bot.core.fixes import fix_thumbnail
+from bot.core.db.database import db
 
 
-@Client.on_callback_query()
-async def cb_handlers(c: Client, cb: "types.CallbackQuery"):
-    if cb.data == "showSettings":
-        await cb.answer()
-        await show_settings(cb.message)
-    elif cb.data == "showThumbnail":
-        thumbnail = await db.get_thumbnail(cb.from_user.id)
-        if not thumbnail:
-            await cb.answer("You didn't set any custom thumbnail!", show_alert=True)
+async def handle_big_rename(
+    c: Client,
+    m: Message,
+    file_id: Union[
+        "raw.types.InputFileBig",
+        "raw.types.InputFile"
+    ],
+    file_name: str,
+    editable: Message,
+    file_type: str
+):
+    await editable.edit("Sending to you ...")
+    upload_as_doc = await db.get_upload_as_doc(m.from_user.id)
+
+    if (upload_as_doc is False) and (file_type == "video"):
+        ttl_seconds = None
+        supports_streaming = m.reply_to_message.video.supports_streaming \
+            if m.reply_to_message.video.supports_streaming \
+            else None
+        duration = m.reply_to_message.video.duration \
+            if m.reply_to_message.video.duration \
+            else 0
+        width = m.reply_to_message.video.width \
+            if m.reply_to_message.video.width \
+            else 0
+        height = m.reply_to_message.video.height \
+            if m.reply_to_message.video.height \
+            else 0
+        mime_type = m.reply_to_message.video.mime_type \
+            if m.reply_to_message.video.mime_type \
+            else "video/mp4"
+        _f_thumb = m.reply_to_message.video.thumbs[0] \
+            if m.reply_to_message.video.thumbs \
+            else None
+        _db_thumb = await db.get_thumbnail(m.from_user.id)
+        thumbnail_file_id = _db_thumb \
+            if _db_thumb \
+            else (_f_thumb.file_id
+                  if _f_thumb
+                  else None)
+        if thumbnail_file_id:
+            await editable.edit("Fetching Thumbnail ...")
+            thumb_path = await c.download_media(thumbnail_file_id,
+                                                f"{Config.DOWNLOAD_DIR}/{m.from_user.id}/{m.message_id}/")
+            if _db_thumb:
+                thumb_path = await fix_thumbnail(thumb_path)
+            thumb = await c.save_file(path=thumb_path)
         else:
-            await cb.answer()
-            await c.send_photo(cb.message.chat.id, thumbnail, "Custom Thumbnail",
-                               reply_markup=types.InlineKeyboardMarkup([[
-                                   types.InlineKeyboardButton("Delete Thumbnail",
-                                                              callback_data="deleteThumbnail")
-                               ]]))
-    elif cb.data == "deleteThumbnail":
-        await db.set_thumbnail(cb.from_user.id, None)
-        await cb.answer("Okay, I deleted your custom thumbnail. Now I will apply default thumbnail.", show_alert=True)
-        await cb.message.delete(True)
-    elif cb.data == "setThumbnail":
-        await cb.answer()
-        await cb.message.edit("Send me any photo to set that as custom thumbnail.\n\n"
-                              "Press /cancel to cancel process.")
-        from_user_thumb: "types.Message" = await c.listen(cb.message.chat.id)
-        if not from_user_thumb.photo:
-            await cb.message.edit("Process Cancelled!")
-            return await from_user_thumb.continue_propagation()
+            thumb = None
+
+        media = raw.types.InputMediaUploadedDocument(
+            mime_type=mime_type,
+            file=file_id,
+            ttl_seconds=ttl_seconds,
+            thumb=thumb,
+            attributes=[
+                raw.types.DocumentAttributeVideo(
+                    supports_streaming=supports_streaming,
+                    duration=duration,
+                    w=width,
+                    h=height
+                ),
+                raw.types.DocumentAttributeFilename(file_name=file_name)
+            ]
+        )
+
+    elif (upload_as_doc is False) and (file_type == "audio"):
+        _f_thumb = m.reply_to_message.audio.thumbs[0] \
+            if m.reply_to_message.audio.thumbs \
+            else None
+        _db_thumb = await db.get_thumbnail(m.from_user.id)
+        thumbnail_file_id = _db_thumb \
+            if _db_thumb \
+            else (_f_thumb.file_id
+                  if _f_thumb
+                  else None)
+        if thumbnail_file_id:
+            await editable.edit("Fetching Thumbnail ...")
+            thumb_path = await c.download_media(thumbnail_file_id,
+                                                f"{Config.DOWNLOAD_DIR}/{m.from_user.id}/{m.message_id}/")
+            if _db_thumb:
+                thumb_path = await fix_thumbnail(thumb_path)
+            thumb = await c.save_file(path=thumb_path)
         else:
-            await db.set_thumbnail(cb.from_user.id, from_user_thumb.photo.file_id)
-            await cb.message.edit("Okay!\n"
-                                  "Now I will apply this thumbnail to next uploads.",
-                                  reply_markup=types.InlineKeyboardMarkup(
-                                      [[types.InlineKeyboardButton("Show Settings",
-                                                                   callback_data="showSettings")]]
-                                  ))
-    elif cb.data == "setCustomCaption":
-        await cb.answer()
-        await cb.message.edit("Okay,\n"
-                              "Send me your custom caption.\n\n"
-                              "Press /cancel to cancel process.")
-        user_input_msg: "types.Message" = await c.listen(cb.message.chat.id)
-        if not user_input_msg.text:
-            await cb.message.edit("Process Cancelled!")
-            return await user_input_msg.continue_propagation()
-        if user_input_msg.text and user_input_msg.text.startswith("/"):
-            await cb.message.edit("Process Cancelled!")
-            return await user_input_msg.continue_propagation()
-        await db.set_caption(cb.from_user.id, user_input_msg.text.markdown)
-        await cb.message.edit("Custom Caption Added Successfully!",
-                              reply_markup=types.InlineKeyboardMarkup(
-                                  [[types.InlineKeyboardButton("Show Settings",
-                                                               callback_data="showSettings")]]
-                              ))
-    elif cb.data == "triggerApplyCaption":
-        await cb.answer()
-        apply_caption = await db.get_apply_caption(cb.from_user.id)
-        if not apply_caption:
-            await db.set_apply_caption(cb.from_user.id, True)
+            thumb = None
+        mime_type = m.reply_to_message.audio.mime_type \
+            if m.reply_to_message.audio.mime_type \
+            else "audio/mpeg"
+        duration = m.reply_to_message.audio.duration \
+            if m.reply_to_message.audio.duration \
+            else None
+        performer = m.reply_to_message.audio.performer \
+            if m.reply_to_message.audio.performer \
+            else None
+        title = m.reply_to_message.audio.title \
+            if m.reply_to_message.audio.title \
+            else None
+
+        media = raw.types.InputMediaUploadedDocument(
+            mime_type=mime_type,
+            file=file_id,
+            force_file=None,
+            thumb=thumb,
+            attributes=[
+                raw.types.DocumentAttributeAudio(
+                    duration=duration,
+                    performer=performer,
+                    title=title
+                ),
+                raw.types.DocumentAttributeFilename(file_name=file_name)
+            ]
+        )
+
+    elif (upload_as_doc is True) or (file_type == "document"):
+        _f_thumb = m.reply_to_message.document.thumbs[0] \
+            if m.reply_to_message.document.thumbs \
+            else None
+        _db_thumb = await db.get_thumbnail(m.from_user.id)
+        thumbnail_file_id = _db_thumb \
+            if _db_thumb \
+            else (_f_thumb.file_id
+                  if _f_thumb
+                  else None)
+        if thumbnail_file_id:
+            await editable.edit("Fetching Thumbnail ...")
+            thumb_path = await c.download_media(thumbnail_file_id,
+                                                f"{Config.DOWNLOAD_DIR}/{m.from_user.id}/{m.message_id}/")
+            if _db_thumb:
+                thumb_path = await fix_thumbnail(thumb_path)
+            thumb = await c.save_file(path=thumb_path)
         else:
-            await db.set_apply_caption(cb.from_user.id, False)
-        await show_settings(cb.message)
-    elif cb.data == "triggerApplyDefaultCaption":
-        await db.set_caption(cb.from_user.id, None)
-        await cb.answer("Okay, now I will keep default caption.", show_alert=True)
-        await show_settings(cb.message)
-    elif cb.data == "showCaption":
-        caption = await db.get_caption(cb.from_user.id)
-        if not caption:
-            await cb.answer("You didn't set any custom caption!", show_alert=True)
-        else:
-            await cb.answer()
-            await cb.message.edit(
-                text=caption,
-                parse_mode="Markdown",
-                reply_markup=types.InlineKeyboardMarkup([[
-                    types.InlineKeyboardButton("Go Back", callback_data="showSettings")
-                ]])
-            )
-    elif cb.data == "triggerUploadMode":
-        await cb.answer()
-        upload_as_doc = await db.get_upload_as_doc(cb.from_user.id)
-        if upload_as_doc:
-            await db.set_upload_as_doc(cb.from_user.id, False)
-        else:
-            await db.set_upload_as_doc(cb.from_user.id, True)
-        await show_settings(cb.message)
-    elif cb.data == "showFileInfo":
-        replied_m = cb.message.reply_to_message
-        _file_name = get_media_file_name(replied_m)
-        text = f"**File Name:** `{_file_name}`\n\n" \
-               f"**File Extension:** `{_file_name.rsplit('.', 1)[-1].upper()}`\n\n" \
-               f"**File Type:** `{get_file_type(replied_m).upper()}`\n\n" \
-               f"**File Size:** `{humanbytes(get_media_file_size(replied_m))}`\n\n" \
-               f"**File MimeType:** `{get_file_attr(replied_m).mime_type}`"
-        await cb.message.edit(
-            text=text,
-            parse_mode="Markdown",
-            disable_web_page_preview=True,
-            reply_markup=types.InlineKeyboardMarkup(
-                [[types.InlineKeyboardButton("Close Message", callback_data="closeMessage")]]
+            thumb = None
+        mime_type = m.reply_to_message.document.mime_type \
+            if m.reply_to_message.document.mime_type \
+            else "application/zip"
+
+        media = raw.types.InputMediaUploadedDocument(
+            mime_type=mime_type,
+            file=file_id,
+            force_file=None,
+            thumb=thumb,
+            attributes=[
+                raw.types.DocumentAttributeFilename(file_name=file_name)
+            ]
+        )
+    else:
+        return await editable.edit("I can't rename it!")
+
+    reply_markup = m.reply_to_message.reply_markup \
+        if m.reply_to_message.reply_markup \
+        else None
+    _db_caption = await db.get_caption(m.from_user.id)
+    apply_caption = await db.get_apply_caption(m.from_user.id)
+    if (not _db_caption) and (apply_caption is True):
+        caption = m.reply_to_message.caption.markdown \
+            if m.reply_to_message.caption \
+            else "**Developer: @AbirHasan2005**"
+    elif _db_caption and (apply_caption is True):
+        caption = _db_caption
+    else:
+        caption = ""
+    parse_mode = "Markdown"
+
+    try:
+        r = await c.send(
+            raw.functions.messages.SendMedia(
+                peer=await c.resolve_peer(m.chat.id),
+                media=media,
+                silent=None,
+                reply_to_msg_id=None,
+                random_id=c.rnd_id(),
+                schedule_date=None,
+                reply_markup=await reply_markup.write(c) if reply_markup else None,
+                **await utils.parse_text_entities(c, caption, parse_mode, None)
             )
         )
-    elif cb.data == "closeMessage":
-        await cb.message.delete(True)
+        await rm_dir(f"{Config.DOWNLOAD_DIR}/{m.from_user.id}/{m.message_id}/")
+    except Exception as _err:
+        Config.LOGGER.getLogger(__name__).error(_err)
+        Config.LOGGER.getLogger(__name__).info(f"{traceback.format_exc()}")
+    else:
+        await editable.edit("Uploaded Successfully!")
