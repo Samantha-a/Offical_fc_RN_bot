bbe5a31e2fcdcb673e5a93d2a5c326e1a5e95aee
Albin <95078987+Samantha-a@users.noreply.github.com>
2022-02-27 18:16:35 +0530

    Update ping.py

--- bot/plugins/ping.py
+++ bot/plugins/ping.py
@@ -13,7 +13,7 @@ async def ping_handler(c: Client, m: "types.Message"):
     await add_user_to_database(c, m)
     await c.send_flooded_message(
         chat_id=m.chat.id,
-        text="𝙷𝙴𝚈 {message.from_user.mention}, I am FC Rename Bot!\n\n"
+        text="𝙷𝙴𝚈 {message.from_user.mention},I am FC Rename Bot!\n\n"
              "I can rename media without downloading it!\n"
              "Speed depends on your media DC.\n\n"
              "Just send me media and reply to it with /rename command.",
