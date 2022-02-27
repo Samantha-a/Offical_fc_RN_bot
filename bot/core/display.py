8293936acb23175310ccc725cc3e2e43bc5a917d
Albin <95078987+Samantha-a@users.noreply.github.com>
2022-02-27 12:27:07 +0530

    Update display.py

--- bot/core/display.py
+++ bot/core/display.py
@@ -9,11 +9,11 @@ from pyrogram.types import Message, CallbackQuery
 from pyrogram.errors import FloodWait
 
 PROGRESS = """
-⏳ **Percentage:** `{0}%`
+⏳ **Loading:** `{0}%`
 ✅ **Done:** `{1}`
 💠 **Total:** `{2}`
 📶 **Speed:** `{3}/s`
-🕰 **ETA:** `{4}`
+🕰 **Time:** `{4}`
 """
 
 
